6bb7d1c51c335a54fe576122714299e6ee361cc3
Alex <olexandrpasalsky@gmail.com>
2021-04-19 02:08:02 +0300

    add struct "Pixel_triplet"

--- Lab4/Lab4/BMPstruct.h
+++ Lab4/Lab4/BMPstruct.h
@@ -4,9 +4,11 @@
 using namespace std;
 struct BMP_head
 {
-	int8_t id[2];				// ім'я файла дві літери "B" "M"
+	int8_t id1;				// ім'я файла дві літери "B" "M"
+	int8_t id2;		
 	int32_t fileSize;			// розмір файлу
-	int16_t reserved[2];		// невикористані повинні бути 0
+	int16_t reserved1;			// невикористані повинні бути 0
+	int16_t reserved2;
 	int32_t headerSize;			// зміщення до опису пікселів (розмір Header і Image Header) (54B для 24-бітних)
 	int32_t infoSize;			// розмір заголовка (Image Header) (40B для 24-бітних)
 	int32_t width;				// ширина зображення в пікселях
@@ -21,3 +23,9 @@ struct BMP_head
 	int32_t biClrImportant;		// к-ть знащущих кольрів (0)
 }; 
 
+struct Pixel_triplet
+{
+	int8_t redComponent;
+	int8_t greenComponent;
+	int8_t blueComponent;
+};
--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -1,8 +1,19 @@
 ﻿#include <iostream>
+#include <fstream>
+#include "BMPstruct.h"
 
+using namespace std;
 int main()
 {
-    
+    /*BMP_head a;
+    ifstream fin("bmp.bmp", ios::binary);
+    fin.read((char*)&a, sizeof(BMP_head));
+    fin.close();
+
+	cout  << a.id1 << " " << a.id2 << " " << a.fileSize << " " << a.reserved1 << " " << a.reserved2 << " ";
+	cout << a.headerSize << " " << a.infoSize << " " << a.width << " " << a.height << " " << a.biplanes << " " << a.bitsPixel << " ";
+	cout << a.biCompression << " " << a.biSizeImage << " " << a.biXPelsPerMeter << " " << a.biYPelsPerMeter << " " << a.biClrUsed << " " << a.biClrImportant << endl;*/
+
     return 0;
 }
 
