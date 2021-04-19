3599207403b594d6681ed7b204795ac87d6277d5
Alex <olexandrpasalsky@gmail.com>
2021-04-19 15:58:00 +0300

    test read struct

--- Lab4/Lab4/BMPstruct.h
+++ Lab4/Lab4/BMPstruct.h
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+#pragma pack(push, 1)
 struct BMP_head
 {
-	int8_t id1;				// ім'я файла дві літери "B" "M"
-	int8_t id2;		
+	int8_t  id[2];				// ім'я файла дві літери "B" "M"
 	int32_t fileSize;			// розмір файлу
-	int16_t reserved1;			// невикористані повинні бути 0
-	int16_t reserved2;
+	int16_t reserved[2];		// невикористані повинні бути 0
 	int32_t headerSize;			// зміщення до опису пікселів (розмір Header і Image Header) (54B для 24-бітних)
 	int32_t infoSize;			// розмір заголовка (Image Header) (40B для 24-бітних)
 	int32_t width;				// ширина зображення в пікселях
@@ -22,10 +21,13 @@ struct BMP_head
 	int32_t biClrUsed;			// записи кольорової картки (0)
 	int32_t biClrImportant;		// к-ть знащущих кольрів (0)
 }; 
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 struct Pixel_triplet
 {
 	int8_t redComponent;
 	int8_t greenComponent;
 	int8_t blueComponent;
 };
+#pragma pack(pop)
--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -5,14 +5,24 @@
 using namespace std;
 int main()
 {
-    /*BMP_head a;
-    ifstream fin("bmp.bmp", ios::binary);
-    fin.read((char*)&a, sizeof(BMP_head));
-    fin.close();
+    BMP_head a;
+    ifstream f;
+    if (f)
+    f.open("bmp.bmp" , ios::binary);
+    if (f.is_open())
+    {
+        cout << true << endl;
+        f.read((char*)&a, sizeof(BMP_head));
 
-	cout  << a.id1 << " " << a.id2 << " " << a.fileSize << " " << a.reserved1 << " " << a.reserved2 << " ";
-	cout << a.headerSize << " " << a.infoSize << " " << a.width << " " << a.height << " " << a.biplanes << " " << a.bitsPixel << " ";
-	cout << a.biCompression << " " << a.biSizeImage << " " << a.biXPelsPerMeter << " " << a.biYPelsPerMeter << " " << a.biClrUsed << " " << a.biClrImportant << endl;*/
+        cout << sizeof(BMP_head) << endl;
+        cout << a.id[0] << " " << a.id[1] << " " << a.fileSize << " " << a.reserved[0] << " " << a.reserved[1] << " ";
+        cout << a.headerSize << " " << a.infoSize << " " << a.width << " " << a.height << " " << a.biplanes << " " << a.bitsPixel << " ";
+        cout << a.biCompression << " " << a.biSizeImage << " " << a.biXPelsPerMeter << " " << a.biYPelsPerMeter << " " << a.biClrUsed << " " << a.biClrImportant << endl;
+
+        f.close();
+    }
+
+    
 
     return 0;
 }
